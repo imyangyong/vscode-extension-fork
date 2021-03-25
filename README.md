39d601b674a2e5b364d18bfbe78b43ab809fa907
杨勇 <1994101600@qq.com>
2021-03-25 20:33:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fork
+
+This extension provide a simple way to use fork app.
+
+## Features
+
+- quick open current workspace in fork app.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 0.0.1
+
+Initial release
+
+### For more information
+
+* [Github](https://github.com/imyangyong/vscode-extension-fork/issues)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,40 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+const cp = require('child_process')
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "fork" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('fork.open', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// https://stackoverflow.com/questions/39569993/vs-code-extension-get-full-path
+		let rootPath: string = ''
+		if (vscode.workspace.workspaceFolders !== undefined) {
+			rootPath = vscode.workspace.workspaceFolders[0].uri.path;
+		}
+		else {
+			vscode.window.showErrorMessage('Fork error: Working folder not found, open a folder an try again');
+			return;
+		}
+		cp.exec('open -a fork ' + rootPath, (err: any, stdout: any, stderr: any) => {
+			if (err) {
+				vscode.window.showErrorMessage('Fork error: ' + err);
+			}
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
