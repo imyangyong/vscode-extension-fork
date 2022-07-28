c8af9a1994052571075c4b082b67a36e14fb41f6
杨勇 <1994101600@qq.com>
2022-07-28 11:52:38 +0800

    refactor: *

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-const cp = require('child_process')
+import type { ExtensionContext } from 'vscode';
+import { commands, workspace, window } from 'vscode';
+const cp = require('child_process');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -14,21 +15,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fork.open', () => {
+	let disposable = commands.registerCommand('fork.open', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// https://stackoverflow.com/questions/39569993/vs-code-extension-get-full-path
-		let rootPath: string = ''
-		if (vscode.workspace.workspaceFolders !== undefined) {
-			rootPath = vscode.workspace.workspaceFolders[0].uri.path;
+		let rootPath: string = '';
+		if (workspace.workspaceFolders !== undefined) {
+			rootPath = workspace.workspaceFolders[0].uri.path;
 		}
 		else {
-			vscode.window.showErrorMessage('Fork error: Working folder not found, open a folder an try again');
+			window.showErrorMessage('Fork error: Working folder not found, open a folder an try again');
 			return;
 		}
 		cp.exec('open -a fork ' + rootPath, (err: any, stdout: any, stderr: any) => {
 			if (err) {
-				vscode.window.showErrorMessage('Fork error: ' + err);
+				window.showErrorMessage('Fork error: ' + err);
 			}
 		});
 	});
