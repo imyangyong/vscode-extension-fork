ad73343c47fb170e7e183ca8342b6ff925ee961e
杨勇 <1994101600@qq.com>
2025-05-06 18:57:10 +0800

    chore: update deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,12 +5,6 @@ export default defineConfig({
     'src/index.ts',
   ],
   format: ['cjs'],
-  shims: false,
-  dts: false,
-  clean: true,
-  env: {
-    NODE_ENV: process.env.NODE_ENV || 'production',
-  },
   external: [
     'vscode',
   ],
